c4503a8a0e41b5ce3139da995bacfe4b4a27ca17
Diana-ai-art <dianagrigoreva313@gmail.com>
2025-05-12 13:25:12 +0300

    Add files via upload

--- Diana_progect/scripts/script.js
+++ Diana_progect/scripts/script.js
@@ -2,4 +2,22 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Скрипт отработал корректно');
+
+//3.2 
+
+    //3.3
+    const intensiveImg = document.querySelectorAll('.services__image');
+    intensiveImg.forEach((item, index) => {
+        const intensiveText = document.querySelectorAll('.services__text');
+            item.addEventListener('mouseenter', () => {
+                      item.style.opacity = 0.5;
+                      intensiveText[index].removeAttribute('hidden');
+    });
+    item.addEventListener('mouseleave', () => {
+              item.style.opacity = 1;
+
+      intensiveText[index].setAttribute('hidden', true);
+    });
+});
+
 });
